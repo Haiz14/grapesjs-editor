a2003d08c26c7cd794d7191d45450460e3c46180
Haiz14 <haiz.gigs@gmail.com>
2023-09-10 11:53:39 +0530

    Loaded meter block

--- addBlocks.js
+++ addBlocks.js
@@ -1,9 +1,11 @@
 import iframe from "./blocks/iframe.js";
 import range from "./blocks/range.js";
+import meter from "./blocks/meter.js";
 
 export async function addExtraBlocks(editor){
 	addBlock(iframe, editor)
 	addBlock(range, editor)
+	addBlock(meter, editor)
 }
 
 async function addBlock(block, editor){
