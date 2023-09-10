b714a02305417364a6f5c3bf9ee125cf62075d4c
Haiz14 <haiz.gigs@gmail.com>
2023-09-10 14:18:05 +0530

    FIXED: Branch name issue and added more files to ignore

--- prod.js
+++ prod.js
@@ -1,16 +1,14 @@
 #!/bin/env node
 import {execSync} from 'child_process'
 
-const IGNORE = ["dist", ".gitignore"];
-const BRANCH_TO_SAVE = "docs";
+const IGNORE = ["dist", ".gitignore", "LICENSE", "README.md"];
+const BRANCH_TO_SAVE = "prod";
 
 // get filenames to delete
 function getFilesToDelete() {
 		const files = execSync("ls").toString().split("\n");
 		return files.filter(file => !IGNORE.includes(file));
 }
-console.log("Deleting files.." + getFilesToDelete());
-// exit
 
 // Build npm run build
 console.log("Installing dependencies...")
@@ -20,9 +18,16 @@ console.log("Building...")
 execSync("npm run build");
 
 console.log("Moving to doc branch...")
-execSync("git checkout docs");
+execSync("git checkout " + BRANCH_TO_SAVE);
 execSync("rm -rf " + getFilesToDelete().join(" "));
 execSync("mv dist/* .");
+execSync("rm -rf dist");
+
+// push
+console.log("Pushing to github...")
+execSync("git add .");
+execSync("git commit -m 'prod: UPDATED production site'");
+execSync("git push");
 
 
 
