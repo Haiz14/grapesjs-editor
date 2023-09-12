cb7ebd522b2b1f865edc6b0d4b97f8b2e310d642
Haiz14 <haiz.gigs@gmail.com>
2023-09-12 08:16:35 +0530

    Added typedefs and ts-check

--- addBlocks.js
+++ addBlocks.js
@@ -1,8 +1,15 @@
+//@ts-check
 import iframe from "./blocks/iframe.js";
-import range from "./blocks/range.js";
+import range, { blockConfig } from "./blocks/range.js";
 import meter from "./blocks/meter.js";
 import canvas from "./blocks/canvas.js";
+import * as Types from './types.js';
 
+/**
+ * @function addExtraBlocks
+ * @param {import('grapesjs').Editor} editor - editor instance
+ * @description add extra blocks to the Editor
+ */
 export async function addExtraBlocks(editor){
 	addBlock(iframe, editor)
 	addBlock(range, editor)
@@ -10,8 +17,16 @@ export async function addExtraBlocks(editor){
 	addBlock(canvas, editor)
 }
 
-async function addBlock(block, editor){
-	editor.BlockManager.add(block.name, block.data)
-	editor.DomComponents.addType(block.name, block.trait)
+/**
+ * @function addBlock
+ * @param {Types.blockConfig} block - block Object
+ * @param {import('grapesjs').Editor} editor - editor instance
+ */
+function addBlock(block, editor){
+	editor.BlockManager.add(block.name, block.blockProperties)
+	editor.DomComponents.addType(block.name, block.componentType)
+	if (Object.prototype.hasOwnProperty('customTrait')) {
+		editor.TraitManager.addType(block.name, block.customTrait)
+	}
 }
 
---
+++ types.js
@@ -0,0 +1,10 @@
+// doesnt work properly
+/**
+ * @typedef {object} blockConfig
+ * @property {string} name
+ * @property {import("grapesjs").AddComponentTypeOptions} componentType
+ * @property {import("grapesjs").BlockProperties} blockProperties
+ * @property {import("grapesjs").CustomTrait} [customTrait]
+ */
+
+export const Types = {}
